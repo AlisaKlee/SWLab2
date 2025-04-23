57ac641e43d07e4ff4559da7e202003383a72c6e
Alicia Fleischmann <afleischma@stud.hs-heilbronn.de>
2025-04-23 23:33:13 +0200

    Added addpatients to index.js

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '../views/LoginView.vue'; 
+import AddPatient from '../views/AddPatient.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Login',
     component: LoginView
+  },
+  {
+    path: '/add patient',
+    name: 'Add Patient',
+    component: AddPatient
   }
 ];
 
