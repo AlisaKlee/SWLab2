b16649a10d90460b977392f532e9a7c23f0a2868
Lilli Gabler <lgabler@stud.hs-heilbronn.de>
2025-05-04 12:36:36 +0200

    SideBar Implementierung und Anpassung der App.vue

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-view></router-view> 
+  <div class="flex">
+    <SideBar v-if="route.path !== '/login'" />
+    <div class="flex-1">
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-};
+<script setup>
+import { useRoute } from 'vue-router'
+import SideBar from './components/SideBar.vue'
+const route = useRoute()
 </script>
