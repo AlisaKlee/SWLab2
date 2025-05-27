27c50b0fbffe718c5e46da2cde8e174188c1a40e
Alisa <akleemann@stud.hs-heilbronn.de>
2025-05-27 15:27:31 +0200

    Fix SPA routing with 404.html and Vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/SWLab2/',  // ← GANZ WICHTIG: dein Repository-Name mit Slash vorne und hinten
   plugins: [vue()],
+  server: {
+    historyApiFallback: true, // hilft lokal beim Testen
+  }
 })
