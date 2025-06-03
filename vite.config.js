5770db8369b11018508b792dde5e5a75bf4e5ac3
Alisa <akleemann@stud.hs-heilbronn.de>
2025-06-03 16:08:30 +0200

    Fix WebSocket config and connect frontend to local backend

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/SWLab2/',  // ← GANZ WICHTIG: dein Repository-Name mit Slash vorne und hinten
   plugins: [vue()],
+  define: {
+    global: 'window', // <- das behebt den Fehler
+  },
   server: {
     historyApiFallback: true, // hilft lokal beim Testen
   }
+  
 })
